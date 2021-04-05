9831638e6e1528bbb0ae873eea6867314558f44c
aswin567 <[toaswink@gmail.com]>
2021-04-05 23:01:32 +0530

    End to end testcases added

---
+++ cypress/integration/search/search.spec.js
@@ -0,0 +1,45 @@
+context('Searching comment', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/')
+    });
+    it('check search field and button', () => {
+        cy.get('#searchShowBtn').click();
+        cy.get('#searchInput').should('be.visible');
+        cy.get('#sendBtn').should('be.visible');
+    });
+    it('[Data Avilable] check search with results', () => {
+        cy.get('#searchShowBtn').click();
+
+        cy.get('#searchInput')
+            .type('ele', { delay: 100 });
+        cy.get('.result-item').should('be.visible');
+        cy.get('#clearBtn').click();
+
+        cy.get('#searchInput')
+            .type('bre', { delay: 100 });
+        cy.get('.result-item').should('be.visible');
+        cy.get('#clearBtn').click();
+
+
+        cy.get('#searchInput')
+            .type('el', { delay: 100 });
+        cy.get('#sendBtn').click();
+        cy.get('.result-item').should('be.visible');
+    });
+    it('[No Data Avilable] check search field and button in', () => {
+        cy.get('#searchShowBtn').click();
+        cy.get('#searchInput').should('be.visible');
+        cy.get('#sendBtn').should('be.visible');
+        cy.get('#searchInput')
+            .type('#', { delay: 100 });
+        cy.get('#sendBtn').click();
+        cy.get('#noData').should('be.visible');
+        cy.get('#clearBtn').click();
+
+
+        cy.get('#searchInput')
+            .type('some random data ##$$##', { delay: 100 });
+
+        cy.get('#noData').should('be.visible');
+    });
+})
